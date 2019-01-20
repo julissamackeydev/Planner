e84c8658d4e70362fc6652220dfa2be1365d548a
julissamackeydev <julissamackeydev@gmail.com>
2019-01-20 16:50:04 -0500

    Installed mongoose, cors

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express'),
 cors = require('cors'),
+mongoose =  require('mongoose')
 app = express();
-app.use(cors())
+app.use(cors());
+mongoose.connect('mongodb://localhost/PlannerDev');
 app.listen(8000, ()=>{
   console.log('server listening on 8000');
 })
 
 app.route('/api/schedule').get((req,res)=>{
     res.send({
-        "activities": [{
+        "schedule": [{
             timestamp: "",
             activity: "",
             status:'In progress'
