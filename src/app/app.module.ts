d03e6f3850695e4da81c8929f2100814189dc216
julissamackeydev <julissamackeydev@gmail.com>
2019-01-21 13:42:04 -0500

    Created schedule component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 import { NavigateComponent } from './navigate/navigate.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigateComponent
+    NavigateComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/schedule/schedule.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule',
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.css']
+})
+export class ScheduleComponent implements OnInit {
+  hours: String[] = ["5am", "5:30am", "6am", "6:30am", "7am", "7:30am", "8am", "8:30am", "9am", "9:30am" , "10am", "10:30am", "11am", "11:30am", "12pm", "12:30pm", "1pm", "1:30pm", "2pm", "2:30pm", "3pm", "3:30pm", "4pm", "4:30pm", "5pm", "5:30pm", "6pm", "6:30pm", "7pm", "7:30pm", "8pm", "8:30pm", "9pm", "9:30pm" , "10pm", "10:30pm", "11pm", "11:30pm"]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
